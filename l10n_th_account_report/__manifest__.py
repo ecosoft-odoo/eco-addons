73c45383cd40a6652f800308562bbfacc924f941
Kitti U <kittiu@gmail.com>
2019-01-31 13:50:38 +0700

    [ADD] l10n_th_partner, l10n_th_account_report

---
+++ l10n_th_account_report/__manifest__.py
@@ -0,0 +1,24 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+
+{'name': 'TH Accounting Reports',
+ 'version': '11.0.0.0.0',
+ 'author': 'Ecosoft,Odoo Community Association (OCA)',
+ 'website': '???',
+ 'license': 'AGPL-3',
+ 'category': 'Accounting',
+ 'depends': ['account',
+             'account_invoice_tax_detail',
+             'xlsx_imexrpt',
+             'date_range',
+             'l10n_th_partner',
+             ],
+ 'data': ['security/ir.model.access.csv',
+          'views/menuitems.xml',
+          'report_vat/report_vat.xml',
+          'report_vat/templates.xml',
+          ],
+ 'installable': True,
+ 'development_status': '???',
+ 'maintainers': ['kittiu'],
+ }
