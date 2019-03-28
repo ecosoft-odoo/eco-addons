d5b92be01f964439e64f1ad508f801ec3a4f7f92
Saran L <saranp502@hotmail.com>
2019-03-28 15:45:31 +0700

    [ENH] pattern email for project

---
+++ eco_project/__manifest__.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+
+{'name': 'Ecosoft Project Management',
+ 'version': '12.0.1.0.0',
+ 'author': 'Ecosoft',
+ 'license': 'AGPL-3',
+ 'category': 'Project',
+ 'depends': ['project'],
+ 'data': [],
+ 'installable': True,
+ 'development_status': 'alpha',
+ 'maintainers': ['saran lim.'],
+ }
