18d7f304bc8b242586d0283a62c91ff91d8e9336
Saran L <saranp502@hotmail.com>
2019-04-04 10:31:41 +0700

    [ENH] module eco_project_support_package

---
+++ eco_project_support_package/models/project.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class Project(models.Model):
+    _inherit = "project.project"
+
+    task_line_ids = fields.One2many(
+        'account.analytic.line',
+        'project_id',
+        readonly=True,
+        help='Get all task in this project.'
+    )
+
+    support_package_line_ids = fields.One2many(
+        'project.support.package.line',
+        'project_id',
+        help='Add Support Package Man-Hour.'
+    )
+    total_man_hour = fields.Float(
+        'Total Man-Hour',
+        compute='_compute_man_hour',
+        readonly=True,
+        help='Total Man-Hour in Support Package.'
+    )
+    used_man_hour = fields.Float(
+        'Used Man-Hour',
+        compute='_compute_man_hour',
+        readonly=True,
+        help='Total Used Man-Hour.'
+    )
+    balance_man_hour = fields.Float(
+        'Balance',
+        compute='_compute_man_hour',
+        readonly=True,
+        help='Balance Total Man-Hour - Total Used Man-Hour'
+    )
+
+    @api.multi
+    def _compute_man_hour(self):
+        for rec in self:
+            rec.total_man_hour = \
+                sum(rec.support_package_line_ids.mapped('duration'))
+            rec.used_man_hour = sum(rec.task_line_ids.mapped('unit_amount'))
+            rec.balance_man_hour = rec.total_man_hour - rec.used_man_hour
