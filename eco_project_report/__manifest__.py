ca183b91a0b05d4a09ade46aedda468217f9fb75
Saran L <saranp502@hotmail.com>
2019-04-05 16:43:18 +0700

    [ENH] eco_project_report

---
+++ eco_project_report/__manifest__.py
@@ -0,0 +1,17 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+
+{'name': 'Ecosoft Project Report',
+ 'version': '12.0.1.0.0',
+ 'author': 'Ecosoft',
+ 'license': 'AGPL-3',
+ 'category': 'Project',
+ 'depends': ['excel_import_export',
+             'project'],
+ 'data': ['report_project/report_project.xml',
+          'report_project/templates.xml',
+          ],
+ 'installable': True,
+ 'development_status': 'alpha',
+ 'maintainers': ['Saran Lim.'],
+ }
